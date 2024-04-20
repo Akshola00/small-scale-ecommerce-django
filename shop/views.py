3632d4a4a642c824e291dde37eb35960dbd636af
Akshola00 <akinniyishola07@gmail.com>
2024-04-20 18:59:50 +0100

    modified:   shop/__pycache__/views.cpython-311.pyc 	new file:   shop/static/style/style.css 	modified:   shop/templates/shop/index.html 	modified:   shop/views.py

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,21 @@
+
 from django.shortcuts import render
 from .models import *
+from django.core.paginator import Paginator
+
 # Create your views here.
 def index(request):
     prod_obj = Products.objects.all()
-    context = {
-       'prod_obj':prod_obj 
-    }
-    return render(request, 'shop/index.html', context)
+
+    # search code
+    item_name = request.GET.get("item-name")
+    print("lorem ipsum dolor sit amet: v", item_name)
+    if item_name != "" and item_name is not None:
+        prod_obj = prod_obj.filter(title__icontains=item_name)
+
+   # paginator code
+    paginator =Paginator(prod_obj, 4)
+    
+
+    context = {"prod_obj": prod_obj}
+    return render(request, "shop/index.html", context)
