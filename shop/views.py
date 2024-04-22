a4cb97e9fe9475ccf04058b0b019624aec573122
Akshola00 <akinniyishola07@gmail.com>
2024-04-22 22:00:53 +0100

    modified:   db.sqlite3 	modified:   ecom/__pycache__/urls.cpython-311.pyc 	modified:   ecom/urls.py 	modified:   shop/__pycache__/views.cpython-311.pyc 	modified:   shop/static/style/style.css 	new file:   shop/templates/shop/detail.html 	modified:   shop/templates/shop/index.html 	modified:   shop/views.py

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
-
 from django.shortcuts import render
 from .models import *
 from django.core.paginator import Paginator
+
 
 # Create your views here.
 def index(request):
@@ -13,9 +13,16 @@ def index(request):
     if item_name != "" and item_name is not None:
         prod_obj = prod_obj.filter(title__icontains=item_name)
 
-   # paginator code
-    paginator =Paginator(prod_obj, 4)
-    
+    # paginator code
+    paginator = Paginator(prod_obj, 4)
+    page = request.GET.get("page")
+    prod_obj = paginator.get_page(page)
 
     context = {"prod_obj": prod_obj}
     return render(request, "shop/index.html", context)
+
+
+def detail(request, id):
+    prod_det = Products.objects.filter(id=id).first()
+    context = {"details": prod_det}
+    return render(request, "shop/detail.html", context)
